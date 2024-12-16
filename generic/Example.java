cc7f39f0420be98ac836ea31b3d7f12721dc13ee
Vinay Dahariya <87851946+vinaydahariya@users.noreply.github.com>
2024-12-16 18:57:08 +0530

    Delete generic directory

--- generic/Example.java
+++
@@ -1,9 +0,0 @@
-package generic;
-
-public class Example {
-    public static void main(String[] args) {
-
-        Box box = new Box("water");
-        System.out.println(box.getValue());
-    }
-}
