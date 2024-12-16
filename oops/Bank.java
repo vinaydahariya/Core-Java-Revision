41f269bf7b0d7a37303ad5e1739acec832b2239f
Vinay Dahariya <87851946+vinaydahariya@users.noreply.github.com>
2024-12-16 18:57:35 +0530

    Delete oops directory

--- oops/Bank.java
+++
@@ -1,7 +0,0 @@
-package oops;
-
-public interface Bank {
-    void interest();
-    void loan();
-    void charges();
-}
--- oops/Parent.java
+++
@@ -1,5 +0,0 @@
-package oops;
-
-public class Parent {
-    String car="TATA";
-}
